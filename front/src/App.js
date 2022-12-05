9357c178bac7da07534423b9c612fff5ca3b2685
DA-WIT <totw5701@naver.com>
2022-12-05 21:21:13 +0900

    useEffect 개념
    state가 변하면 component 전채가 다시 render된다.
    
    useEffect(f, [])
    두번째 파라미터인 array에 입력한 state가 변할때만 f 가 실행되도록 할 수 있다.

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,26 @@
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("always run");
+  // useEffect : 첫 로딩, 그리고 2번째 parameter에 들어가는 state가 변했을 때만 render한다.
+  useEffect(() => { 
+    console.log("I run only once.");
+  }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes", keyword);
+  }, [keyword]); 
+  useEffect(() => {
+    console.log("I run when 'counter' changes");
+  }, [counter]); 
   return (
     <div>
-      <h1>Welcome back!!</h1>
+      <input onChange={onChange} type="text" placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
