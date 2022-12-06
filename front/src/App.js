57e8dc9a49fd3d3ce348e05a2f8d112a736b689e
DA-WIT <totw5701@naver.com>
2022-12-06 11:14:07 +0900

    Practice - ToDoList

--- front/src/App.js
+++ front/src/App.js
@@ -1,27 +1,33 @@
 import { useState, useEffect } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("created :>");
-    return () => console.log("destroyed :<"); 
-    /* 
-      Cleanup function : component가 없어질 때(unmount될 때) 실행되는 function.
-      useEffect(f, array)의 f의 리턴함수로 받는다.
-      메모리 누수를 방지하기위해 말그대로 clean up을 위해 사용되는 경우가 많다.
-
-    */
-  }, []);
-  return <h1>Hello</h1>
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((prevArr) => [toDo, ...prevArr]);
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "HIDE" : "SHOW"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do ..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
     </div>
   );
 }
