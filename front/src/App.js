394365be9ffa80577efbdeedefddb3e24d55e6aa
DA-WIT <totw5701@naver.com>
2022-12-06 10:36:06 +0900

    CleanUp function 개념
    Cleanup function : component가 없어질 때(unmount될 때) 실행되는 function.
          useEffect(f, array)의 f의 리턴함수로 받는다.
          메모리 누수를 방지하기위해 말그대로 clean up을 위해 사용되는 경우가 많다.

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,27 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("always run");
-  // useEffect : 첫 로딩, 그리고 2번째 parameter에 들어가는 state가 변했을 때만 render한다.
-  useEffect(() => { 
-    console.log("I run only once.");
+function Hello() {
+  useEffect(() => {
+    console.log("created :>");
+    return () => console.log("destroyed :<"); 
+    /* 
+      Cleanup function : component가 없어질 때(unmount될 때) 실행되는 function.
+      useEffect(f, array)의 f의 리턴함수로 받는다.
+      메모리 누수를 방지하기위해 말그대로 clean up을 위해 사용되는 경우가 많다.
+
+    */
   }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes", keyword);
-  }, [keyword]); 
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]); 
+  return <h1>Hello</h1>
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+
   return (
     <div>
-      <input onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "HIDE" : "SHOW"}</button>
     </div>
   );
 }
