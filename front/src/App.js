599f7b15b74a982cc079f4dbc3f2a004b15275a3
DA-WIT <totw5701@naver.com>
2022-12-07 22:19:19 +0900

    practice - coin price calculator

--- front/src/App.js
+++ front/src/App.js
@@ -1,33 +1,59 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDo("");
-    setToDos((prevArr) => [toDo, ...prevArr]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [idx, setIdx] = useState(0);
+  const [money, setMoney] = useState("");
+  
+
+  const onChange = (event) => {
+    setMoney(event.target.value);
   };
+  const onChangeSelect = (event) => {
+    setIdx(event.target.value);
+  };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers") // https://goodteacher.tistory.com/540
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      <div>
         <input
+          type="number"
           onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do ..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      {toDos.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
+          value={money}
+          placeholder="USD"
+        ></input>
+      </div>
+      <div>
+        {loading ? (
+          <strong>Loading...</strong>
+        ) : (
+          <select value={idx} onChange={onChangeSelect}>
+            {coins.map((coin, index) => (
+              <option key={coin.id} value={index}>
+                {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+        )}
+      </div>
+      <div>
+        {loading ? (
+            <strong>Loading...</strong>
+          ) : (
+            1 / coins[idx].quotes.USD.price * money 
+          )
+        } COINS
+      </div>
     </div>
   );
 }
